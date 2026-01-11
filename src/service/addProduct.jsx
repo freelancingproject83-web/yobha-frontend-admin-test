54280a5dbc5c47122d065ed8fc8ce6903ea99efe
toolevatech-Aman <tooleva.tech@gmail.com>
2026-01-11 15:58:20 +0530

    check

--- src/service/addProduct.jsx
+++ src/service/addProduct.jsx
@@ -1,6 +1,6 @@
 import { PostMultipart } from "./axiosService";
 
 export const AddProductCSV = async (file) => {
-  const response = await PostMultipart("/Product/bulk-upload", file);
+  const response = await PostMultipart("/Products/bulk-upload", file);
   return response.data;
 };
